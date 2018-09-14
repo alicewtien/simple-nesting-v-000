8a4bf9fa70bbedae20e7b4ebe847bde470406999
alice T <alicewtien@gmail.com>
2018-09-14 18:06:58 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -105,7 +105,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
 programmer_hash[:alan_kay][:known_for] = alans_new_info
-puts changing_alan[:alan_kay][:known_for]
+puts programmer_hash[:alan_kay][:known_for]
 
 end
 
@@ -126,5 +126,5 @@ def adding_to_dennis
         }
      }
 programmer_hash[:dennis_ritchie][:languages] << "Assembly"
-puts adding_to_dennis[:dennis_ritchie][:languages][1]
+puts programmer_hash[:dennis_ritchie][:languages][1]
 end
